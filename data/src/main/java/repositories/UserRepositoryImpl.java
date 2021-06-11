2155b94424a762c01ba22dd9ae73c0734b32bbc7
miri <miriyungreis@gmail.com>
2021-06-11 03:35:29 +0300

    update

---
+++ data/src/main/java/repositories/UserRepositoryImpl.java
@@ -0,0 +1,64 @@
+package repositories;
+
+import com.example.Entity.User;
+import com.google.firebase.auth.AuthResult;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import Entity.UserEntity;
+import Entity.mapper.UserDataMapper;
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+import repositories.datasource.CloudUserDataSource;
+
+@Singleton
+public class UserRepositoryImpl implements UserRepository{
+    private CloudUserDataSource cloudUserDataSource;
+    private UserDataMapper userDataMapper;
+    @Inject
+    public UserRepositoryImpl(CloudUserDataSource cloudUserDataSource) {
+        this.cloudUserDataSource = cloudUserDataSource;
+        this.userDataMapper = new UserDataMapper();
+  }
+//    public String addUser(User user) throws Exception {
+//        return cloudUserDataSource.insert(UserDataMapper.transform(user));
+//    }
+//    @Override
+//    public Observable<User> getUser(String userId, boolean fromServer) {
+//        return null;
+//    }
+
+    @Override
+    public Observable<User> getUser(String userId, boolean fromServer) {
+        return null;
+    }
+
+    @Override
+    public Observable<User> login(final String password,
+                                       final String email) {
+        return this.cloudUserDataSource.login(new UserEntity(password, email))
+                .map(new Function<AuthResult, UserEntity>() {
+                    @Override
+                    public UserEntity apply(AuthResult authResult) throws Exception {
+                        return UserDataMapper.transform(authResult);
+                    }
+                })
+                .map(new Function<UserEntity, User>() {
+                    @Override
+                    public User apply(UserEntity userEntity) throws Exception {
+                        return UserDataMapper.transform(userEntity);
+                    }
+                });
+    }
+
+    @Override
+    public void saveUser(User user) {
+
+    }
+
+//    @Override
+//    public void saveUser(User user) {
+//
+//    }
+}
--- domain/src/main/java/useCases/SignInUseCase.java
+++
@@ -1,8 +0,0 @@
-package useCases;
-
-public class SignInUseCase {
-
-
-
-
-}
