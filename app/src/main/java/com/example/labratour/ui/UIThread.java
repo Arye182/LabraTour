189ff1aa0250c04e345b6cf8ccf0ca9d981f9bf7
miri <miriyungreis@gmail.com>
2021-06-13 01:36:59 +0300

    update

--- app/src/main/java/com/example/labratour/ui/UIThread.java
+++ app/src/main/java/com/example/labratour/ui/UIThread.java
@@ -1,1 +1,16 @@
+package com.example.labratour.ui;
 
+import com.example.executors.PostExecutionThread;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+public class UIThread implements PostExecutionThread{
+
+
+
+public UIThread() {}
+
+@Override public Scheduler getScheduler() {
+        return AndroidSchedulers.mainThread();
+        }
+}
---
+++ data/src/main/java/JobExecutor.java
@@ -0,0 +1,28 @@
+import com.example.executors.ExecutionThread;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class JobExecutor implements ExecutionThread {
+    private final ThreadPoolExecutor threadPoolExecutor;
+
+
+    public JobExecutor() {
+        this.threadPoolExecutor = new ThreadPoolExecutor(3, 5, 10, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(), new JobThreadFactory());
+    }
+
+    @Override public void execute(Runnable runnable) {
+        this.threadPoolExecutor.execute(runnable);
+    }
+
+    private static class JobThreadFactory implements ThreadFactory {
+        private int counter = 0;
+
+        @Override public Thread newThread( Runnable runnable) {
+            return new Thread(runnable, "android_" + counter++);
+        }
+    }
+}
