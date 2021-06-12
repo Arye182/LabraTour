6854fe770816ec970a5f14ba209c0171e13d2265
miri <miriyungreis@gmail.com>
2021-06-13 01:08:13 +0300

    update

--- data/src/main/java/repositories/datasource/UserDataSourceFactory.java
+++ data/src/main/java/repositories/datasource/UserDataSourceFactory.java
@@ -3,7 +3,6 @@ package repositories.datasource;
 import com.google.firebase.auth.FirebaseAuth;
 import com.repositories.datasource.CloudUserDataSource;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
@@ -12,7 +11,7 @@ public class UserDataSourceFactory {
     private final FirebaseAuth firebaseAuth;
 
 
-    @Inject
+
     UserDataSourceFactory(FirebaseAuth firebaseAuth) {
         this.firebaseAuth = firebaseAuth;
 
