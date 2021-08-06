0f39d602634a3492967f91b8b790d1e81ed9fd64
miri <miriyungreis@gmail.com>
2021-08-06 17:29:43 +0300

    update

--- data/src/main/java/com/example/labratour/data/Entity/mapper/NearbyPlaceJsonMapper.java
+++ data/src/main/java/com/example/labratour/data/Entity/mapper/NearbyPlaceJsonMapper.java
@@ -1,12 +1,14 @@
 package com.example.labratour.data.Entity.mapper;
 
+import com.example.labratour.data.Entity.MyNearbyPlaces;
 import com.example.labratour.data.Entity.NearbyPlaceResult;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NearbyPlaceJsonMapper {
     private final Gson gson;
@@ -15,13 +17,20 @@ public class NearbyPlaceJsonMapper {
     public NearbyPlaceJsonMapper() {
         this.gson = new Gson();
     }
-    public NearbyPlaceResult transformUserEntity(String singleNearbyPlaceResponse) throws JsonSyntaxException {
+    public NearbyPlaceResult transformNearbyPlaceResult(String singleNearbyPlaceResponse) throws JsonSyntaxException {
         final Type nearbyPlaceType = new TypeToken<NearbyPlaceResult>() {}.getType();
         return this.gson.fromJson(singleNearbyPlaceResponse, nearbyPlaceType);
     }
-    public  List<NearbyPlaceResult> transformCollection(String response)
+    public  ArrayList<NearbyPlaceResult> transformCollection(String response)
          throws JsonSyntaxException {
-            final Type nearbyPlaceType = new TypeToken<List<NearbyPlaceResult>>() {}.getType();
-            return this.gson.fromJson(response, nearbyPlaceType);
+            final Type nearbyPlaceType = new TypeToken<MyNearbyPlaces>() {}.getType();
+            MyNearbyPlaces nearbyPlaces = this.gson.fromJson(response, nearbyPlaceType);
+            return new ArrayList<NearbyPlaceResult>(Arrays.asList(nearbyPlaces.getResults()));}
+//        final ArrayList<NearbyPlaceResult> places = new ArrayList<>(20);
+//        for (String n :arr) {
+//            final NearbyPlaceResult place = transformNearbyPlaceResult(n);
+//            if (place != null){
+//                places.add(place);}
+//        }
+//        return places;}
     }
-}
