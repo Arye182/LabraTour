9b3954a4c790400cfe184ca8ff6628e741cc1124
Arye Amsalem <arye.amsalem@gmail.com>
2021-09-12 20:16:17 +0300

    plan of search sub categories to search page

--- app/src/main/java/com/example/labratour/presentation/utils/Constants.kt
+++ app/src/main/java/com/example/labratour/presentation/utils/Constants.kt
@@ -2,4 +2,167 @@ package com.example.labratour.presentation.utils
 
 object Constants {
     const val USERS: String = "users"
+
+    val typesMap: Map<String, String> = mapOf(
+        // Transportation
+        "airport" to "Airport",
+        "bus_station" to "Bus Station",
+        "car_rental" to "Car Rental",
+        "gas_station" to "Gas Station",
+        "light_rail_station" to "Light Train",
+        "subway_station" to "Subway",
+        "taxi_stand" to "Taxi",
+        "train_station" to "Train Staion",
+
+        // Parks
+        "amusement_park" to "Amusement_Park",
+        "aquarium" to "Aquarium",
+        "zoo" to "Zoo",
+        "park" to "Parks",
+
+        // Art & Culture
+        "art_gallery" to "Art Galleryt",
+        "library" to "Library",
+        "museum" to "Museum",
+
+        // Money
+        "atm" to "ATM",
+        "bank" to "Bank",
+
+        // Food and Drinks
+        "bakery" to "Bakery",
+        "bar" to "Bar",
+        "cafe" to "Cafe",
+        "liquor_store" to "Liquers",
+        "restaurant" to "Restaurant",
+        "food" to "Food",
+        "supermarket" to "Supermarket",
+
+        // Shopping
+        "clothing_store" to "Clothing Store",
+        "convenience_store" to "Convenience Store",
+        "book_store" to "Book Store",
+        "electronics_store" to "Electronics",
+        "jewelry_store" to "Jewelries",
+        "department_store" to "Department Store",
+        "home_goods_store" to "Home Goods",
+        "shopping_mall" to "Mall",
+        "shoe_store" to "Shoes",
+        "store" to "Store",
+
+        // Religon
+        "cemetery" to "Cemetery",
+        "church" to "Church",
+        "hindu_temple" to "Hindu Temple",
+        "synagogue" to "Synagouge",
+        "mosque" to "Mosque",
+
+        // medical
+        "doctor" to "Doctor",
+        "drugstore dentist" to "Drugstore Dentist",
+        "hospital" to "Hospital",
+        "pharmacy" to "Pharmacy",
+
+        // fun
+        "casino" to "Casino",
+        "movie_theater" to "Movie Theatre",
+        "night_club" to "Night Club",
+
+        // Sports
+        "gym" to "GYM",
+        "spa" to "Spa",
+        "stadium" to "Stadium",
+
+        // General
+        "point_of_interest" to "Point Of Interest",
+        "tourist_attraction" to "Attraction",
+    )
 }
+
+// banned
+// accounting
+// beauty_salon
+// bicycle_store
+// fire_station
+// bowling_alley
+// campground
+// car_dealer
+// car_repair
+// car_wash
+// embassy
+// city_hall
+// electrician
+// furniture_store
+// courthouse
+// lawyer
+// hardware_store
+// florist
+// funeral_home
+// hair_care
+// insurance_agency
+// roofing_contractor
+// local_government_office
+// locksmith
+// laundry
+// town_square
+// lodging
+// meal_delivery
+// meal_takeaway
+// movie_rental
+// moving_company
+// painter
+// parking
+// pet_store
+// physiotherapist
+// plumber
+// police
+// post_office
+// primary_school
+// university
+// real_estate_agency
+// ןfinance
+// street_address
+// street_number
+// political
+// post_box
+// postal_code
+// postal_code_prefix
+// postal_code_suffix
+// postal_town
+// premise
+// room
+// route
+// sublocality
+// sublocality_level_1
+// sublocality_level_2
+// sublocality_level_3
+// sublocality_level_4
+// sublocality_level_5
+// subpremise
+// rv_park
+// school
+// secondary_school
+// locality
+// natural_feature
+// neighborhood
+// place_of_worship
+// plus_code
+// administrative_area_level_1
+// administrative_area_level_2
+// administrative_area_level_3
+// administrative_area_level_4
+// administrative_area_level_5
+// intersection
+// travel_agency
+// archipelago
+// colloquial_area
+// continent
+// country
+// establishment
+// floor
+// general_contractor
+// health
+// landmark
+// geocode
+// storage
+// "transit_station"
