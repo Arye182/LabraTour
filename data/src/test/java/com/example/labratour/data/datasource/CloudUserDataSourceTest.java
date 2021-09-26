9a587ca06c4b331c8450bab75a613ceb88097271
miri <miriyungreis@gmail.com>
2021-09-26 05:39:15 +0300

    update

---
+++ data/src/test/java/com/example/labratour/data/datasource/CloudUserDataSourceTest.java
@@ -0,0 +1,19 @@
+package com.example.labratour.data.datasource;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+
+import java.net.MalformedURLException;
+
+public class CloudUserDataSourceTest extends TestCase {
+
+    private final String email = "test10@gmail.com";
+    private final String password = "123123";
+    @Before
+    public void setUp() throws MalformedURLException, NoSuchFieldException {}
+    public void testRegister() {
+
+    }
+
+}
