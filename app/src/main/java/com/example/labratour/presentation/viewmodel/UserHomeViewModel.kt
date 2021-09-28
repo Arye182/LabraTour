330e0249ae803e74c716e0b8833651044c152125
Arye Amsalem <arye.amsalem@gmail.com>
2021-09-29 02:04:15 +0300

    ui

--- app/src/main/java/com/example/labratour/presentation/viewmodel/UserHomeViewModel.kt
+++ app/src/main/java/com/example/labratour/presentation/viewmodel/UserHomeViewModel.kt
@@ -114,7 +114,7 @@ class UserHomeViewModel(
             Log.i("Places", "NearbyPlacesStringListFetcherObserver Observer - On Error: " + exception.message)
             // isLoading.postValue(false)
             // logInTaskStatus.postValue(false)
-            error.postValue("NearbyPlacesStringListFetcherObserver Observer - On Error: " +exception.message)
+            error.postValue("NearbyPlacesStringListFetcherObserver Observer - On Error: " + exception.message)
         }
         override fun onNext(value: ArrayList<String>) {
             Log.i("Places", "NearbyPlacesStringListFetcherObserver Observer - On Next...")
@@ -134,7 +134,7 @@ class UserHomeViewModel(
         }
         override fun onError(exception: Throwable) {
             Log.i("Places", "RankPlace Observer - On Error: " + exception.message)
-            error.postValue(exception.message)
+            error.postValue("RankPlace Observer - On Error: " + exception.message)
         }
         override fun onNext(value: String) {
             Log.i("Places", "RankPlace Observer - On Next message: $value")
