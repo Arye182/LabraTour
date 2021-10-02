530a36114231eb1a4a4ce40c7d39e333c553e0cf
miri <miriyungreis@gmail.com>
2021-10-03 02:28:52 +0300

    update

---
+++ domain/src/main/java/com/example/labratour/domain/useCases/GetNearbyPlacesByTypeUseCase.java
@@ -0,0 +1,44 @@
+package com.example.labratour.domain.useCases;
+
+import android.util.Log;
+
+import com.example.labratour.domain.Entity.NearbyPlaceEntity;
+import com.example.labratour.domain.executors.ExecutionThread;
+import com.example.labratour.domain.executors.PostExecutionThread;
+import com.example.labratour.domain.repositories.NearbyPlacesRepository;
+import com.fernandocejas.arrow.checks.Preconditions;
+
+import io.reactivex.Observable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class GetNearbyPlacesByTypeUseCase  extends NearbyPlacesUseCase{
+
+
+  public GetNearbyPlacesByTypeUseCase(NearbyPlacesRepository nearbyPlacesRepository, ExecutionThread executionThread, PostExecutionThread postExecutionThread) {
+    super(nearbyPlacesRepository, executionThread, postExecutionThread);
+  }
+
+  public void execute(DisposableObserver observer, String lat, String lon, String type) {
+      Preconditions.checkNotNull(observer);
+      final Observable<NearbyPlaceEntity
+              > observable =
+              this.buildUseCaseObservable(lat,  lon, type)
+                      .subscribeOn(Schedulers.from(executionThread))
+                      .observeOn(postExecutionThread
+                              .getScheduler());
+      addDisposable(observable.subscribeWith(observer));
+    }
+
+
+    public Observable<NearbyPlaceEntity> buildUseCaseObservable(String lat, String lon, String type) {
+      Log.i("testNearbyTypeUseCase", "lat:"+lat+ "lon:" + lon+ type);
+
+      return repository.get(lat, lon, type);
+    }
+
+
+
+}
+
+
